0e1f2d7fda4114c0b5c35f1908ad0e1ef13b7628
Victor <victor.chawsukho@aterio.io>
2024-06-17 05:49:16 +0000

    set up file structure and project name

--- models/test_query.sql
+++ models/test_query.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='table')}}
 
 select price, bathrooms, bedrooms, property_type 
-from {{source('demo_dbt_link_fivetran', 'demo_documentation') }}
+from {{ source('demo_dbt_link_fivetran', 'demo_documentation') }}
 where price = "45"
